e8453bae50f11792104ab69ba6d1b57c1a4a2f20
hnmike <nnamno2612@gmail.com>
2025-05-15 10:14:26 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Import Database
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    
+
+    implementation("com.google.firebase:firebase-database:21.0.0")
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     
@@ -67,6 +70,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime:2.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("org.jsoup:jsoup:1.17.2")
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
 }
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
